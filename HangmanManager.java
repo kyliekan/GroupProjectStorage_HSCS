cea6e494f0b9728f447bf6d5fd09ce2b42428df0
kyliekan <92802910+kyliekan@users.noreply.github.com>
2021-10-19 09:11:47 -0500

    Add files via upload

---
+++ HangmanManager.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class HangmanManager
+{
+	public HangmanManager( List<String> dictionary, int length, int max )
+	{
+		
+	}
+	
+	public Set<String> words()
+	{
+		return null;
+	}	
+	
+	public int guessesLeft()
+	{
+		return 0;
+	}
+		
+	public Set<Character> guesses()
+	{
+		return null;
+	}
+	
+	public String pattern()
+	{
+		return "";
+	}
+	
+	public int record( char guess )
+	{
+		return 0;
+	}
+}
